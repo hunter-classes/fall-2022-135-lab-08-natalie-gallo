9faad70b41786c4c0e8072e937d06d612c54e066
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-10-25 22:45:51 -0400

    experimenting, added two cpp files

---
+++ funcs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "funcs.h"
+#include "imageio.h"
+
+//TASK A
+//invert.cpp
+
+void invert(std::string filename){ //not sure what parameters...
+
+std::string input = "image1.pgm";
+
+int img[MAX_H][MAX_W];
+int h, w;
+
+readImage(input, img, h, w);
+
+int out[MAX_H][MAX_W];
+
+//if pixel is white, turn black. if pixel is black, turn white.
+//where orginal pixel value if "neg" + 255 ex. -1 (aka 1) -> 254 and -254 (aka 254) -> 1
+
+int color;
+
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w; col++) {
+      color = img[row][col];
+      color = -color;
+      img[row][col] = color + 255;
+      out[row][col] = img[row][col];
+    }
+  }
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "funcs.h"
+#include "imageio.h"
+
+
+int main()
+{
+
+ 
+  return 0;
+}
