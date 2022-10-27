b33e9d5a1fd638a23cd0bed7d37e6b653886f722
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-10-26 20:52:47 -0400

    task b done?

--- funcs.cpp
+++ funcs.cpp
@@ -3,7 +3,7 @@
 #include "funcs.h"
 #include "imageio.h"
 
-//TASK A -- WORKS! need to make it into a function...
+//TASK A -- WORKS! need to make it into a function... and change output name!
 //invert.cpp
 
 void invert(std::string filename){ //not sure what parameters...
@@ -31,6 +31,41 @@ void invert(std::string filename){ //not sure what parameters...
     }
   }
 
-  writeImage("outImage.pgm",out, h, w);
+  writeImage("taskA.pgm",out, h, w);
 
 }
+
+//TASK B -- works(?) need to turn into function and change output file name
+
+  std::string input = "image1.pgm";
+
+  int img[MAX_H][MAX_W];
+  int h, w;
+
+  readImage(input, img, h, w);
+  
+  int out[MAX_H][MAX_W];
+
+  //if pixel is white, turn black. if pixel is black, turn white.
+  //where orginal pixel value if "neg" + 255 ex. -1 (aka 1) -> 254 and -254 (aka 254) -> 1
+
+  int color;
+
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col <= w/2; col++) {
+      out[row][col] = img[row][col];
+    }
+  }
+
+  for(int row = 0; row < h; row++) {
+    for(int col = w/2; col < w; col++) {
+      color = img[row][col];
+      color = -color;
+      img[row][col] = color + 255;
+      out[row][col] = img[row][col];
+    }
+  }
+
+  writeImage("outImage.pgm",out, h, w);
+
+//TASK C
