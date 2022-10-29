9314eeae977b8eec054e0ba3b6fb477f1e4b70bd
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-10-28 20:45:40 -0400

    task c done?

--- funcs.cpp
+++ funcs.cpp
@@ -66,6 +66,58 @@ void invert(std::string filename){ //not sure what parameters...
     }
   }
 
-  writeImage("outImage.pgm",out, h, w);
+  writeImage("taskB.pgm",out, h, w);
 
-//TASK C
+//TASK C -- works... need to make proper implemation per lab instructions
+
+  std::string input = "image1.pgm";
+
+  int img[MAX_H][MAX_W];
+  int h, w;
+
+  readImage(input, img, h, w);
+  
+  int out[MAX_H][MAX_W];
+
+  //if pixel is white, turn black. if pixel is black, turn white.
+  //where orginal pixel value if "neg" + 255 ex. -1 (aka 1) -> 254 and -254 (aka 254) -> 1
+
+  //left side
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w/4; col++) {
+      out[row][col] = img[row][col];
+    }
+  }
+
+  //top
+  for(int row = 0; row < h/4; row++) {
+    for(int col = 0; col < w; col++) {
+      out[row][col] = img[row][col];
+    }
+  }
+
+  //bottom
+  for(int row = h - h/4; row < h; row++) {
+    for(int col = 0; col < w; col++) {
+      out[row][col] = img[row][col];
+    }
+  }
+
+  //right side
+  for(int row = 0; row < h; row++) {
+    for(int col = w - w/4; col < w; col++) {
+      out[row][col] = img[row][col];
+    }
+  }
+
+  //middle
+  for(int row = h/4; row < h * 0.75; row++) {
+    for(int col = w/4; col < w * 0.75; col++) {
+      img[row][col] = 255;
+      out[row][col] = img[row][col];
+    }
+  }
+
+writeImage("taskC.pgm",out, h, w);
+
+//TASK D
